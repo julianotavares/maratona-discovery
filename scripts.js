9fec490a164f8c4b4e471ef02c3db95d0af736f3
Juliano Tavares <juliano.tavaress@gmail.com>
2021-01-29 18:57:32 -0300

    format currency

--- scripts.js
+++ scripts.js
@@ -13,6 +13,28 @@ const Modal = {
   }
 } 
 
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021',
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  },
+
+]
+
 const Transaction = {
   incomes() {
     // somar as entradas
@@ -24,3 +46,43 @@ const Transaction = {
     // entradas menos saídas
   }
 }
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  
+  addTransaction(transaction, index){    
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+          <img src="./assets/minus.svg" alt="Remover transação">
+        </td>
+  `
+  
+  return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace()
+
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transactions)
+})
